71d74ad63bca089d6a677b9233d1bbc40feb9a56
Anna Kholkina <kholkina.a2021@gmail.com>
2022-04-15 14:16:03 +0300

    mode error check

--- Успеваемость_студентов/stats_functions/test.py
+++ Успеваемость_студентов/stats_functions/test.py
@@ -1,13 +1,21 @@
-from functions import mean
+from functions import mean, mode
 
 def test_mean():
+    print('Test-case for mean function')
     assert mean([1, 2, 3]) == 2, "Should be 2"
     print('Test 1 passed')
 
-def test_mean_tuple():
     assert mean((1, 2, 3)) == 2, "Should be 2"
     print('Test 2 passed')
+    print('=================================')
+
+def test_mode():
+    print('Test-case for mode function')
+
+    assert mode([1, 1, 2, 2, 3, 4]) == [1, 2], "Should be [1, 2]"
+    print('Test 1 passed')
+    print('=================================')
 
 if __name__ == "__main__":
     test_mean()
-    test_mean_tuple()
+    test_mode()
